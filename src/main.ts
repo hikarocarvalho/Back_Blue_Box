6d5ed17b8cbba77b60e8bf6fec96fdb37ecb5a03
hikarocarvalho <hikarofcarvalho@gmail.com>
2021-10-21 14:32:12 -0300

    up cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { UnauthorizedInterceptor } from './interceptors/unauthorized.interceptor
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
     cors:{
-      origin:['http://localhost:3000','http://localhost:3002','https://projectblueboxfront.herokuapp.com']
+      origin:['http://localhost:3000','179.177.135.197','https://projectblueboxfront.herokuapp.com']
     },
   });
   app.useGlobalPipes(
