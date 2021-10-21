977bd238a2bf749c94a254ab24b04851da1ad137
hikarocarvalho <hikarofcarvalho@gmail.com>
2021-10-21 14:43:17 -0300

    removed cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,7 @@ import { UnauthorizedInterceptor } from './interceptors/unauthorized.interceptor
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{
-    cors:{
-      origin:['http://localhost:3000','179.177.135.197','https://projectblueboxfront.herokuapp.com']
-    },
+    cors:false
   });
   app.useGlobalPipes(
     new ValidationPipe({
